bbff2e26d2e3f3a278c68e3b5f1de74825ee649a
Mohamed Radwan <muhammed.a.radwan@gmail.com>
2023-02-10 15:43:13 +0200

    functions and filters

--- index.php
+++ index.php
@@ -14,21 +14,41 @@
             [
                 'name' => 'Do Androids Dream of Electric Sheep',
                 'author' => 'Philip K. Dick',
+                'releaseYear' => 1968,
                 'purchaseUrl' => 'https://example.com'
             ],
             [
                 'name' => 'Project Hail Mary',
                 'author' => 'Andy Weir',
+                'releaseYear' => 2021,
+                'purchaseUrl' => 'https://example.com'
+            ],
+            [
+                'name' => 'The Martian',
+                'author' => 'Andy Weir',
+                'releaseYear' => 2011,
                 'purchaseUrl' => 'https://example.com'
             ]
         ];
+
+        function filterByauthor($books, $author) {
+            $filteredBooks = [];
+
+            foreach($books as $book) {
+                if ($book['author'] === $author) {
+                    $filteredBooks[] = $book;
+                }
+            }
+
+            return $filteredBooks;
+        }
     ?>
 
     <ul>
-        <?php foreach($books as $book): ?>
+        <?php foreach(filterByauthor($books, 'Andy Weir') as $book): ?>
             <li>
                 <a href="<?php $book['purchaseUrl'] ?>">
-                    <?= $book['name'] ?>
+                    <?= $book['name'] ?> (<?= $book['releaseYear']?>) by <?= $book['author'] ?>
                 </a>
             </li>
         <?php endforeach; ?>
