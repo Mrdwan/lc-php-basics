62de6d7c3eaeb2bc9000f265ca00e78f5d7cac97
Mohamed Radwan <muhammed.a.radwan@gmail.com>
2023-02-10 15:35:09 +0200

    Functions and Filters

--- index.php
+++ index.php
@@ -11,15 +11,26 @@
 
     <?php
         $books = [
-            'Do Androids Dream of Electric Sheep',
-            'The Langoliers',
-            'Hail Mary'
+            [
+                'name' => 'Do Androids Dream of Electric Sheep',
+                'author' => 'Philip K. Dick',
+                'purchaseUrl' => 'https://example.com'
+            ],
+            [
+                'name' => 'Project Hail Mary',
+                'author' => 'Andy Weir',
+                'purchaseUrl' => 'https://example.com'
+            ]
         ];
     ?>
 
     <ul>
         <?php foreach($books as $book): ?>
-            <li><?= $book?></li>
+            <li>
+                <a href="<?php $book['purchaseUrl'] ?>">
+                    <?= $book['name'] ?>
+                </a>
+            </li>
         <?php endforeach; ?>
     </ul>
 
