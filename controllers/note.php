3c37edf994b2539a80156704c5465bfdc6839378
Mohamed Radwan <muhammed.a.radwan@gmail.com>
2023-02-20 17:23:54 +0200

    auth query

--- controllers/note.php
+++ controllers/note.php
@@ -3,8 +3,20 @@
 $heading = 'Notes';
 
 $config = require "config.php";
+require "Response.php";
 
 $db = new Database($config['database']);
-$note = $db->query("select * from notes where id = ?", [$_GET['id']])->fetch();
+$note = $db->query("select * from notes where user_id = :user id = :id", [
+    'user' => 1,
+    'id' => $_GET['id']
+])->fetch();
+
+if (! $note) {
+    abort();
+}
+
+if ($note['user_id'] !== 1) {
+    abort(Response::FORBIDDEN);
+}
 
 require "views/note.view.php";
