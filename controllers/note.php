6fb66341f5f60bcaba5e02ce5a2155112538978a
Mohamed Radwan <muhammed.a.radwan@gmail.com>
2023-02-21 15:22:59 +0200

    refactor

--- controllers/note.php
+++ controllers/note.php
@@ -9,14 +9,9 @@ $db = new Database($config['database']);
 $note = $db->query("select * from notes where user_id = :user id = :id", [
     'user' => 1,
     'id' => $_GET['id']
-])->fetch();
+])->findOrFail();
 
-if (! $note) {
-    abort();
-}
-
-if ($note['user_id'] !== 1) {
-    abort(Response::FORBIDDEN);
-}
+$currentUser = 1;
+authorize($note['user_id'] === $currentUser);
 
 require "views/note.view.php";
