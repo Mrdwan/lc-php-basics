cef9074a93793055e38f5c5141a7cb43cf25d95d
Mohamed Radwan <muhammed.a.radwan@gmail.com>
2023-02-23 17:23:24 +0200

    validations

--- controllers/notes-create.php
+++ controllers/notes-create.php
@@ -1,14 +1,25 @@
 <?php
 
 $heading = 'Create a note';
+$config = require "config.php";
+$db = new Database($config['database']);
+$errors = [];
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $config = require "config.php";
-    $db = new Database($config['database']);
-    $note = $db->query("insert into notes (body, user_id) values (:body, :user_id)", [
-        'body' => $_POST['body'],
-        'user_id' => 1
-    ]);
+    if (! strlen($_POST['body'])) {
+        $errors['body'] = 'Note description is required';
+    }
+
+    if (strlen($_POST['body']) > 1000) {
+        $errors['body'] = 'Description should not be more than 1000 character';
+    }
+
+    if (empty($errors)) {
+        $note = $db->query("insert into notes (body, user_id) values (:body, :user_id)", [
+            'body' => $_POST['body'],
+            'user_id' => 1
+        ]);
+    }
 }
 
 require "views/notes-create.view.php";
